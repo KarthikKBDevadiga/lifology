29c7fa354c43eca4d3587fe841840d8e0d8a8c6d
Karthik K B Devadiga <karthikkbdevadiga@gmail.com>
2021-11-29 17:19:21 +0530

    update flow

--- pages/subscription/[id].js
+++ pages/subscription/[id].js
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 import Constants from '../../helpers/Constants'
 
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 
-import { RadioGroup } from '@headlessui/react'
+import { Dialog, RadioGroup, Transition } from '@headlessui/react'
 
 import classNames from '/helpers/classNames'
 
@@ -12,6 +12,8 @@ import cookies from "next-cookies"
 import { mutateGraph, queryGraph } from "../../helpers/GraphQLCaller"
 import { SubscriptionList, SubscriptionPayment, SchemeGetProfile } from "../../helpers/GraphQLSchemes"
 import LoadingDialog from "../../components/dialog/LoadingDialog"
+import { CheckIcon, ExclamationIcon } from "@heroicons/react/outline"
+import { useRouter } from "next/router"
 
 const client = new ApolloClient({
     uri: Constants.baseUrl + "/api/auth",
@@ -30,9 +32,15 @@ const plans = [
     { name: 'Monthly', description: 'Get unlimited access to all our programs for a month.', price: 'Rs.999/month' }
 ]
 export default function Page22({ plan, token, profile }) {
+    const router = useRouter()
 
     const [selected, setSelected] = useState(plans[0])
     const [loadingDialog, setLoadingDialog] = useState(false)
+
+    const [successDialog, setSuccessDialog] = useState(false)
+    const [successDialogString, setSuccessDialogString] = useState('')
+    const [errorDialog, setErrorDialog] = useState(false)
+    const [errorDialogString, setErrorDialogString] = useState('')
 
     const proceedToPay = event => {
         setLoadingDialog(true)
@@ -224,7 +232,97 @@ export default function Page22({ plan, token, profile }) {
             </div>
 
             <LoadingDialog showDialog={loadingDialog} setShowDialog={setLoadingDialog} />
+            <Transition.Root show={successDialog} as={Fragment}>
+                <Dialog as="div" static className="fixed z-10 inset-0 overflow-y-auto" open={successDialog} onClose={setSuccessDialog}>
+                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                        </Transition.Child>
 
+                        {/* This element is to trick the browser into centering the modal contents. */}
+                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+                            &#8203;
+                        </span>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enterTo="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-4 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-4">
+                                <div>
+                                    <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+                                        <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                                    </div>
+                                    <div className="mt-3 text-center sm:mt-5">
+                                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                                            {successDialogString}
+                                        </Dialog.Title>
+                                        <button className="absolute h-0 w-0 overflow-hidden" />
+                                    </div>
+                                </div>
+                            </div>
+                        </Transition.Child>
+                    </div>
+                </Dialog>
+            </Transition.Root>
+
+            <Transition.Root show={errorDialog} as={Fragment}>
+                <Dialog as="div" static className="fixed z-10 inset-0 overflow-y-auto" open={errorDialog} onClose={setErrorDialog}>
+                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                        </Transition.Child>
+
+                        {/* This element is to trick the browser into centering the modal contents. */}
+                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+                            &#8203;
+                        </span>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enterTo="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+                                <div>
+                                    <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+                                        <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                                    </div>
+                                    <div className="mt-3 text-center sm:mt-5">
+                                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                                            {errorDialogString}
+                                        </Dialog.Title>
+                                        <button className="absolute h-0 w-0 overflow-hidden" />
+                                    </div>
+                                </div>
+                            </div>
+                        </Transition.Child>
+                    </div>
+                </Dialog>
+            </Transition.Root>
         </>
     )
 }
